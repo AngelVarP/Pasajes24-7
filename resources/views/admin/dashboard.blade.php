dee3564b1f557f270517a88863fdf155b2acaa12
LAngel_VP <luis.vargas.p@uni.pe>
2025-10-28 08:28:56 -0500

    actualizacion Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
     {{-- Favicon --}}
-    <link rel="icon" href="{{ asset('images/p-favicon.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('images/logo-pasajes24-7.png') }}" type="image/png">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
