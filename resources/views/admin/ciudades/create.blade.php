c5d976aca9a5502cb5a84e25553702d0d2a85af8
LAngel_VP <luis.vargas.p@uni.pe>
2025-10-29 22:38:22 -0500

    actualizacion ciudades

--- resources/views/admin/ciudades/create.blade.php
+++ resources/views/admin/ciudades/create.blade.php
@@ -161,7 +161,7 @@
                         <div>
                             <label for="nombre" class="block text-sm font-medium text-gray-700 mb-1">Nombre de la Ciudad:</label>
                             <input type="text" name="nombre" id="nombre" required maxlength="100"
-                                    value="{{ old('nombre') }}" placeholder="Ej: Medellín"
+                                    value="{{ old('nombre') }}" placeholder="Ej: Juliaca"
                                     class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm @error('nombre') input-error @enderror">
                             @error('nombre') <p class="mt-1 text-xs text-red-600">{{ $message }}</p> @enderror
                         </div>
@@ -170,7 +170,7 @@
                         <div>
                             <label for="departamento" class="block text-sm font-medium text-gray-700 mb-1">Departamento:</label>
                             <input type="text" name="departamento" id="departamento" required maxlength="100"
-                                    value="{{ old('departamento') }}" placeholder="Ej: Antioquia"
+                                    value="{{ old('departamento') }}" placeholder="Ej: Puno"
                                     class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm @error('departamento') input-error @enderror">
                             @error('departamento') <p class="mt-1 text-xs text-red-600">{{ $message }}</p> @enderror
                         </div>
